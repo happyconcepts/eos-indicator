0a3777dbad1c98c0fe1ebb8a9e636c8341cb9d79
HappyConcepts <happyconcepts.com@gmail.com>
2018-08-17 07:28:07 -0700

    unit test binance

--- test_binance.py
+++ test_binance.py
@@ -1,27 +1,28 @@
 # Unit tests for eos-investor
 # https://github.com/happyconcepts/eos-investor
-# eos-investor copyright 2018 ben bird
 
 # python -m unittest test_binance
 
 import unittest
 import eos_investor
 
+
 class TestEOSIndicator(unittest.TestCase):
 
     def setUpModule():
-	print ("in setUpModule()")
+	    print ("in setUpModule()")
 
-    # test test (reference)
+    # test reference
     def test_Add(self):
-	result = eos_investor.add(10,5)
-	self.assertEqual(result, 15)
+	    result = eos_investor.add(10,5)
+	    self.assertEqual(result, 15)
 
     # test binance API
     def test_Binance(self):
-	b = eos_investor.binance()
-	result = b.run()
-	self.assertTrue(result)
+	    b = eos_investor.binance()
+	    result = b.run()
+	    self.assertTrue(result)
+
 
 if __name__ == '__main__':
     unittest.main()
